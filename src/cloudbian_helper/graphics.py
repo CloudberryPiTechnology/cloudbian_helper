6576fc915b9af0aef74be744ccb07c6f585de598
CloudberryPiTechnologies <cloudberrypi1@outlook.com>
2026-06-09 16:15:57 +0530

    Added 3d graphics along with documentation

--- src/cloudbian_helper/graphics.py
+++ src/cloudbian_helper/graphics.py
@@ -380,3 +380,260 @@ class Graphs:
             dpi=dpi,
             bbox_inches="tight"
         )
+    # -------------------------
+    # 3D LINE GRAPH
+    # -------------------------
+    def line_3d_graph(
+        self,
+        x,
+        y,
+        z,
+        color="blue",
+        linewidth=2,
+        xlabel="X",
+        ylabel="Y",
+        zlabel="Z",
+        show=True
+    ):
+        """Draw a 3D line graph."""
+
+        fig = plt.figure(figsize=self.figsize)
+        ax = fig.add_subplot(111, projection="3d")
+
+        ax.plot(
+            x,
+            y,
+            z,
+            color=color,
+            linewidth=linewidth
+        )
+
+        ax.set_title(self.title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_zlabel(zlabel)
+
+        if show:
+            plt.show()
+
+    # -------------------------
+    # 3D SCATTER GRAPH
+    # -------------------------
+    def scatter_3d_graph(
+        self,
+        x,
+        y,
+        z,
+        color="red",
+        size=50,
+        xlabel="X",
+        ylabel="Y",
+        zlabel="Z",
+        show=True
+    ):
+        """Draw a 3D scatter graph."""
+
+        fig = plt.figure(figsize=self.figsize)
+        ax = fig.add_subplot(111, projection="3d")
+
+        ax.scatter(
+            x,
+            y,
+            z,
+            c=color,
+            s=size
+        )
+
+        ax.set_title(self.title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_zlabel(zlabel)
+
+        if show:
+            plt.show()
+
+    # -------------------------
+    # 3D BAR GRAPH
+    # -------------------------
+    def bar_3d_graph(
+        self,
+        x,
+        y,
+        z,
+        dx=0.5,
+        dy=0.5,
+        color="skyblue",
+        xlabel="X",
+        ylabel="Y",
+        zlabel="Z",
+        show=True
+    ):
+        """Draw a 3D bar graph."""
+
+        fig = plt.figure(figsize=self.figsize)
+        ax = fig.add_subplot(111, projection="3d")
+
+        ax.bar3d(
+            x,
+            y,
+            np.zeros(len(z)),
+            dx,
+            dy,
+            z,
+            color=color,
+            shade=True
+        )
+
+        ax.set_title(self.title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_zlabel(zlabel)
+
+        if show:
+            plt.show()
+
+    # -------------------------
+    # 3D SURFACE GRAPH
+    # -------------------------
+    def surface_3d_graph(
+        self,
+        x,
+        y,
+        z,
+        cmap="viridis",
+        xlabel="X",
+        ylabel="Y",
+        zlabel="Z",
+        show=True
+    ):
+        """Draw a 3D surface graph."""
+
+        fig = plt.figure(figsize=self.figsize)
+        ax = fig.add_subplot(111, projection="3d")
+
+        ax.plot_surface(
+            x,
+            y,
+            z,
+            cmap=cmap,
+            edgecolor="none"
+        )
+
+        ax.set_title(self.title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_zlabel(zlabel)
+
+        if show:
+            plt.show()
+
+    # -------------------------
+    # 3D WIREFRAME GRAPH
+    # -------------------------
+    def wireframe_3d_graph(
+        self,
+        x,
+        y,
+        z,
+        color="black",
+        xlabel="X",
+        ylabel="Y",
+        zlabel="Z",
+        show=True
+    ):
+        """Draw a 3D wireframe graph."""
+
+        fig = plt.figure(figsize=self.figsize)
+        ax = fig.add_subplot(111, projection="3d")
+
+        ax.plot_wireframe(
+            x,
+            y,
+            z,
+            color=color
+        )
+
+        ax.set_title(self.title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_zlabel(zlabel)
+
+        if show:
+            plt.show()
+
+    # -------------------------
+    # 3D CONTOUR GRAPH
+    # -------------------------
+    def contour_3d_graph(
+        self,
+        x,
+        y,
+        z,
+        cmap="viridis",
+        xlabel="X",
+        ylabel="Y",
+        zlabel="Z",
+        show=True
+    ):
+        """Draw a 3D contour graph."""
+
+        fig = plt.figure(figsize=self.figsize)
+        ax = fig.add_subplot(111, projection="3d")
+
+        ax.contour3D(
+            x,
+            y,
+            z,
+            50,
+            cmap=cmap
+        )
+
+        ax.set_title(self.title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_zlabel(zlabel)
+
+        if show:
+            plt.show()
+
+    # -------------------------
+    # 3D STEM GRAPH
+    # -------------------------
+    def stem_3d_graph(
+        self,
+        x,
+        y,
+        z,
+        color="green",
+        xlabel="X",
+        ylabel="Y",
+        zlabel="Z",
+        show=True
+    ):
+        """Draw a 3D stem graph."""
+
+        fig = plt.figure(figsize=self.figsize)
+        ax = fig.add_subplot(111, projection="3d")
+
+        for xi, yi, zi in zip(x, y, z):
+            ax.plot(
+                [xi, xi],
+                [yi, yi],
+                [0, zi],
+                color=color
+            )
+
+        ax.scatter(
+            x,
+            y,
+            z,
+            color=color
+        )
+
+        ax.set_title(self.title)
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_zlabel(zlabel)
+
+        if show:
+            plt.show()
